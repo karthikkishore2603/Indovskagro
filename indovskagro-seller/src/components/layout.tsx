68b38f0f659cb5cdd98785a8c956abd14357b35d
Naveen M K <naveen521kk@gmail.com>
2024-02-02 19:31:17 +0530

    some improvements

--- indovskagro-seller/src/components/layout.tsx
+++ indovskagro-seller/src/components/layout.tsx
@@ -1,16 +1,18 @@
 import React from "react";
 import { Sidebar } from "@/components/sidebar";
+import { Separator } from "./ui/separator";
 
 export function Layout({ children }: { children?: React.ReactNode }) {
   return (
     <>
-      <div className="flex h-100 w-100">
+      <div className="flex h-full w-full">
         <div className="max-w-[200px] w-full h-[100vh] border">
           <Sidebar />
         </div>
-        <div className="ml-2">
+        <div className="w-full h-full">
           <div className="flex h-[52px] items-center justify-center"></div>
-          {children}
+          <Separator />
+          <div className="m-2">{children}</div>
         </div>
       </div>
     </>
--- indovskagro-seller/src/components/sidebar.tsx
+++ indovskagro-seller/src/components/sidebar.tsx
@@ -10,9 +10,8 @@ export function Sidebar() {
   return (
     <>
       <TooltipProvider delayDuration={0}>
-        {" "}
         <div className="flex h-[52px] items-center justify-center w-full">
-          <div className="relative z-20 flex items-center text-sm font-medium px-6">
+          <div className="relative z-20 flex items-center text-sm font-medium">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
@@ -25,7 +24,7 @@ export function Sidebar() {
             >
               <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
             </svg>
-            INDOVSK AGROINDUSTRIES
+            INDOVSK
           </div>
         </div>
         <Separator />
