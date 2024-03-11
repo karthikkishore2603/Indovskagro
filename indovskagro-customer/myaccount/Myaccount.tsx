a62fab15884f2b31dd0580a09f21166c26c3be3f
Karthik Kishore B <karthikkishore2603@gmail.com>
2024-03-11 10:26:17 +0530

    textbox placed

--- indovskagro-customer/myaccount/Myaccount.tsx
+++ indovskagro-customer/myaccount/Myaccount.tsx
@@ -1,10 +1,14 @@
 import React from "react";
-import { Box, Card, Typography } from "@mui/material";
+
+import { Box, Button, Card, Typography } from "@mui/material";
 import TextField from "@mui/material/TextField";
 
-export default function Myaccount() {
+
+export default async function Myaccount() {
+  
   return (
     <>
+    
       <Card
         sx={{
           marginBottom: "20px",
@@ -27,7 +31,13 @@ export default function Myaccount() {
           sx={{
             padding: "20px",
             display: "flex",
-            justifyContent: "space-between",
+            justifyContent: "center",
+            flexDirection: {
+              xs: "column",
+              sm: "column",
+              md: "row",
+              lg: "row",
+            },
           }}
         >
           <TextField
@@ -35,28 +45,199 @@ export default function Myaccount() {
             label="First Name"
             variant="outlined"
             sx={{
-                paddingRight: "20px",
+              paddingBottom: {
+                xs: "20px",
+                sm: "20px",
+                md: "0px",
+                lg: "0px",
+              },
+              paddingRight: {
+                xs: "0px",
+                sm: "0px",
+                md: "20px",
+                lg: "20px",
+              },
               width: {
-                xs: "23px",
-                sm: "45px",
+                xs: "400px",
+                sm: "525px",
                 md: "450px",
-                lg: "500px",
+                lg: "600px",
               },
             }}
           />
           <TextField
-            sx={{              
+            sx={{
               width: {
-                xs: "23px",
-                sm: "45px",
+                xs: "400px",
+                sm: "525px",
                 md: "450px",
-                lg: "500px",
+                lg: "600px",
+              },
+              paddingRight: {
+                xs: "0px",
+                sm: "0px",
+                md: "20px",
+                lg: "20px",
               },
             }}
             id="outlined-basic"
-            label="First Name"
+            label="Last Name"
             variant="outlined"
           />
+        </Box>
+
+        <Box
+          sx={{
+            padding: "20px",
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: {
+              xs: "column",
+              sm: "column",
+              md: "row",
+              lg: "row",
+            },
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Email"
+            variant="outlined"
+            sx={{
+              paddingBottom: {
+                xs: "20px",
+                sm: "20px",
+                md: "0px",
+                lg: "0px",
+              },
+              width: {
+                xs: "400px",
+                sm: "525px",
+                md: "450px",
+                lg: "600px",
+              },
+              paddingRight: {
+                xs: "0px",
+                sm: "0px",
+                md: "20px",
+                lg: "20px",
+              },
+            }}
+          />
+          <TextField
+            id="outlined-basic"
+            label="Phone NO"
+            variant="outlined"
+            sx={{
+              width: {
+                xs: "400px",
+                sm: "525px",
+                md: "450px",
+                lg: "600px",
+              },
+              paddingRight: {
+                xs: "0px",
+                sm: "0px",
+                md: "20px",
+                lg: "20px",
+              },
+            }}
+          />
+        </Box>
+
+        <Box
+          sx={{
+            padding: "20px",
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: {
+              xs: "column",
+              sm: "column",
+              md: "row",
+              lg: "row",
+            },
+          }}
+        >
+          <TextField
+            id="outlined-basic"
+            label="Address"
+            variant="outlined"
+            sx={{
+              width: {
+                xs: "400px",
+                sm: "525px",
+                md: "900px",
+                lg: "1200px",
+              },
+              paddingRight: {
+                xs: "0px",
+                sm: "0px",
+                md: "20px",
+                lg: "20px",
+              },
+            }}
+          />
+        </Box>
+
+        <Box
+          sx={{
+            padding: "20px",
+            display: "flex",
+            justifyContent: "center",
+            flexDirection: {
+              xs: "column",
+              sm: "column",
+              md: "row",
+              lg: "row",
+            },
+          }}
+        >
+          <Button
+            id="outlined-basic"
+            variant="contained"
+            sx={{
+              marginBottom: {
+                xs: "20px",
+                sm: "20px",
+                md: "0px",
+                lg: "0px",
+              },
+              marginRight: {
+                xs: "0px",
+                sm: "0px",
+                md: "20px",
+                lg: "20px",
+              },
+              width: {
+                xs: "400px",
+                sm: "525px",
+                md: "450px",
+                lg: "580px",
+              },
+            }}
+          >
+            Change Password
+          </Button>
+
+          <Button
+            variant="contained"
+            sx={{
+              marginRight: {
+                xs: "0px",
+                sm: "0px",
+                md: "20px",
+                lg: "20px",
+              },
+              width: {
+                xs: "400px",
+                sm: "525px",
+                md: "450px",
+                lg: "580px",
+              },
+            }}
+          >
+            Update
+          </Button>
         </Box>
       </Card>
     </>
