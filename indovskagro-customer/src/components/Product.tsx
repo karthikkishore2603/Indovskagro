cbbd5aebf8add6ff8a12477080b7751db2760740
Karthik Kishore B <karthikkishore2603@gmail.com>
2024-03-13 12:26:59 +0530

    footer and home page alert

--- indovskagro-customer/src/components/Product.tsx
+++ indovskagro-customer/src/components/Product.tsx
@@ -12,6 +12,10 @@ import { useEffect } from "react";
 import { DialerSip } from "@mui/icons-material";
 import { set } from "firebase/database";
 
+import Snackbar from "@mui/material/Snackbar";
+import IconButton from "@mui/material/IconButton";
+import CloseIcon from "@mui/icons-material/Close";
+
 export async function addtocart({
   product,
   setOpen,
@@ -29,44 +33,78 @@ export async function addtocart({
   const userId = await addToCart(user?.uid!, product.id!, 1);
 
   console.log(userId);
+
+  //selectornnn
+  const [openalert, setOpenalert] = React.useState(false);
+
+  const handleClick = () => {
+    setOpenalert(true);
+  };
+
+  const handleClose = (
+    event: React.SyntheticEvent | Event,
+    reason?: string
+  ) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setOpenalert(false);
+  };
 }
 
 export function ProductComponent({ product }: { product: Product }) {
   const user = auth.currentUser;
   const [open, setOpen] = React.useState(false);
+  const [OpenAlready, setOpenAlready] = React.useState(false);
+  const [openLogin, setOpenLogin] = React.useState(false);
   const handleClose = () => {
     setOpen(false);
+    setOpenAlready(false);
+    setOpenLogin(false);
   };
+  
+
+  const action = (
+    <React.Fragment>
+      <IconButton
+        size="small"
+        aria-label="close"
+        color="inherit"
+        onClick={handleClose}
+      >
+        <CloseIcon fontSize="small" />
+      </IconButton>
+    </React.Fragment>
+  );
+
   return (
     <>
-    
-    
       <div className="col-md-6 col-lg-4 col-xl-3">
-      
         <div className="rounded position-relative fruite-item">
-        <a href={`/product/index.html?id=${product.id}`}>
-          <div className="fruite-img">
-            <img 
-              src={product.image}
-              className="img-fluid w-100 rounded-top"
-              alt=""
-            />
-          </div>
-         
-          <div
-            className="text-white bg-secondary px-3 py-1 rounded position-absolute"
-            style={{
-              top: "10px",
-              left: "10px",
-            }}
-          >
-            {product.category}
-          </div>
+          <a href={`/product/index.html?id=${product.id}`}>
+            <div className="fruite-img">
+              <img
+                src={product.image}
+                className="img-fluid w-100 rounded-top"
+                alt=""
+              />
+            </div>
+
+            <div
+              className="text-white bg-secondary px-3 py-1 rounded position-absolute"
+              style={{
+                top: "10px",
+                left: "10px",
+              }}
+            >
+              {product.category}
+            </div>
           </a>
           <div className="p-4 border border-secondary border-top-0 rounded-bottom">
-          <a href={`/product/index.html?id=${product.id}`}>
-            <h4 >{product.title}</h4>
-            <p>{product.description}</p>
+            <a href={`/product/index.html?id=${product.id}`}>
+              <h4>{product.title}</h4>
+              <p>{product.description}</p>
             </a>
             <div className="d-flex justify-content-between flex-lg-wrap">
               <p className="text-dark fs-5 fw-bold mb-0">
@@ -82,18 +120,27 @@ export function ProductComponent({ product }: { product: Product }) {
                   );
                 })(product.price.toString())}
               </p>
-              <a  
+              <a
                 onClick={async () => {
                   console.log(product.id);
                   console.log(auth.currentUser?.uid);
+
+                  if (!auth.currentUser) {
+                    console.log("Please login to add to cart");
+                    setOpenLogin(true);
+                    return;
+                  }
 
                   await getCartItems(auth.currentUser?.uid!);
                   const cartItems = await getCartItems(auth.currentUser?.uid!);
                   if (cartItems.some((item) => item.productId === product.id)) {
                     console.log("Already in to cart");
+                    setOpenAlready(true);
                   } else {
                     await addtocart({ product, setOpen });
                     console.log("Added to cart");
+                    
+                    <></>;
                   }
                 }}
                 className="btn border border-secondary rounded-pill px-3 text-primary"
@@ -103,45 +150,34 @@ export function ProductComponent({ product }: { product: Product }) {
               </a>
             </div>
           </div>
-          
         </div>
       </div>
-   
-      <Dialog
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="alert-dialog-title"
-        aria-describedby="alert-dialog-description"
-      >
-        {!user && (
-          <DialogTitle id="alert-dialog-title">
-            {"Login to Add to Cart"}
-          </DialogTitle>
-        )}
-        {user && (
-          <DialogTitle id="alert-dialog-title">{"Added to Cart"}</DialogTitle>
-        )}
 
-        {!user && (
-          <DialogActions>
-            <Button
-              onClick={() => {
-                window.location.href = "/login/index.html";
-              }}
-              autoFocus
-            >
-              Login
-            </Button>
-          </DialogActions>
-        )}
-        {user && (
-          <DialogActions>
-            <Button onClick={handleClose} autoFocus>
-              Continue
-            </Button>
-          </DialogActions>
-        )}
-      </Dialog>
+      <Snackbar
+        open={openLogin}
+        autoHideDuration={3000}
+        onClose={handleClose}
+        message="Please Login to Add to Cart"
+        action={action}
+      />
+      {user && (
+        <>
+          <Snackbar
+            open={open}
+            autoHideDuration={3000}
+            onClose={handleClose}
+            message="Added to Cart"
+            action={action}
+          />
+          <Snackbar
+            open={OpenAlready}
+            autoHideDuration={3000}
+            onClose={handleClose}
+            message="Already in Cart"
+            action={action}
+          />
+        </>
+      )}
     </>
   );
 }
--- indovskagro-seller/src/pages/login.tsx
+++ indovskagro-seller/src/pages/login.tsx
@@ -7,7 +7,7 @@ import { UserLoginForm } from "@/components/user-login-form";
 export default function Home() {
   return (
     <>
-    <title>INDOVSK AGROINDUSTRIES</title>
+    <title>INDO VSK AGRO INDUSTRIES</title>
       <div className="container relative flex h-[100vh] flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         
         <div className="relative hidden h-full flex-col bg-muted p-10 text-white lg:flex dark:border-r">
@@ -25,7 +25,7 @@ export default function Home() {
             >
               <path d="M15 6v12a3 3 0 1 0 3-3H6a3 3 0 1 0 3 3V6a3 3 0 1 0-3 3h12a3 3 0 1 0-3-3" />
             </svg>
-            INDOVSK AGROINDUSTRIES
+            INDO VSK AGRO INDUSTRIES
           </div>
         </div>
         <div className="lg:p-8">
